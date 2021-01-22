68860f3ae74d4d096bd8dcfc46de74aeab71fef8
Collin Hubbard <49377750+CollinHubbard@users.noreply.github.com>
2021-01-22 12:49:45 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,44 +1,173 @@
-var starCount = 0;
+body {
+    font-family: Roboto;
 
-window.addEventListener("click", func2);
+    margin: 0 auto;
 
-function func2(e) {
-    var newStar = document.createElement("div");
+    background-color: #191919;
 
-    newStar.innerHTML = "★";
-    newStar.setAttribute("class", "stars");
-
-    newStar.style.position = "relative";
-
-    newStar.style.top = (-1300 - (starCount * 100)) + "px";
-
-    newStar.style.left = 10 + "px";
-
-    var newId = "" + starCount;
-
-    newStar.setAttribute("id", newId);
-
-    newStar.style.transform = "translate3d(" + e.clientX + "px, " + e.clientY + "px, 0)";
-  
-    console.log(e.clientX);
-
-    document.body.appendChild(newStar);
-
-    starCount++;
 }
 
-window.addEventListener("mousemove", func);
+.top-banner {
+    position: fixed;
 
-function func(e) {
-    var x = e.clientX;
-    var y = e.clientY;
+    top: 0;
+    left: 0;
+    width: 100%;
 
-    var s = document.querySelectorAll(".stars");
+    background-color: #DFE2DB;
+    height: 20vh;
 
-    s.forEach(function(s) {
-        var delay = s.id * 100;
-        setTimeout(function() {
-            s.style.transform = "translate3d(" + x + "px, " + y + "px, 0)";
-        }, delay);
-    })
+    content: "";
+
+    z-index: 10;
+
+    clip-path: polygon(
+        0 0,
+        100% 0,
+        100% 50%,
+        90% 100%,
+        0 100%
+    );
+
+     background-image: url('street.jpg');
+}
+
+.top-nav {
+    display: flex;
+    position: absolute;
+    left: 45%;
+    top: 20%;
+    transform: translate(-50%, 0);
+}
+
+.p-nav {
+    position: relative;
+
+    top: -1px;
+
+    z-index: 0;
+
+    padding: .25em 10vw;
+    background-color: #DFE2DB;
+
+    white-space: nowrap;
+
+    margin: 0 2em;
+
+    clip-path: polygon(
+        0 0,
+        100% 0,
+        100% 60%,
+        95% 100%,
+        5% 100%,
+        0 60%
+    );
+
+    user-select: none;
+
+    border: none;
+}
+
+.p-nav:hover {
+    background-color: #fff47a;
+}
+
+.p-nav:focus {
+    outline: none;
+}
+
+.descript {
+    height: 60vh;
+    margin: 1.5em 1.5em;
+
+    border: 3px solid black;
+
+    border-radius: 10px;
+
+    padding: .8em;
+
+    background-color: #DFE2DB;
+
+}
+
+.text {
+    font-size: calc(1vh + .75vw);
+
+}
+
+
+#resume {
+    display: block;
+
+    position: absolute;
+
+    top: 100vh;
+
+    left: 23vw;
+
+    transition: all .2s ease-in-out;
+}
+
+#resume:hover {
+    transform: scale(1.1);
+
+    box-shadow: 0 0 60px #4b4b4b;
+
+    transition: all .2s ease-in-out;
+}
+
+.special {
+
+    position: relative;
+
+    display: flex;
+
+    align-items: center;
+    justify-content: center;
+
+    top: 215vh;
+
+    background-color: #191919;
+
+    height: 6vh;
+
+}
+
+.spBtn {
+    background-color: #fff47a;
+
+    height: 8vh;
+    width: 16vw;
+
+    clip-path: polygon(
+        0 0,
+        100% 0,
+        90% 100%,
+        0% 100%,
+        10% 0%
+    );
+
+    transform: scale(1.09);
+
+    border: none;
+
+    transition: all .3s linear;
+
+    font-size: 36px;
+
+    font-family: Ariel;
+
+    text-align: center;
+    text-decoration: none;
+
+}
+
+.spBtn:focus {
+    outline: none;
+
+    background-color: #191919;
+
+    transition: all .3s linear;
+
+    transform: scale(1.15, 1.09);
 }
